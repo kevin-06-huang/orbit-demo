bd04fbce85058224df4d6d05a668aff00dcc065f
Kevin <kevin.06.huang@gmail.com>
2023-08-16 13:16:47 -0700

    Set up react

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import Game from "./components/Game";
+
+const App = () => {
+  return (<></>);
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
