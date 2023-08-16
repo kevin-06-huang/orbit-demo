7e8d1e90935e702013fe25da0676eda9322d79c1
Kevin <kevin.06.huang@gmail.com>
2023-08-16 16:29:07 -0700

    Fix the stacking issue

--- src/components/OrbitContainer.tsx
+++ src/components/OrbitContainer.tsx
@@ -3,15 +3,15 @@ import Moon from "./Moon";
 const OrbitContainer = () => {
   return (
     <div className="ellipse-container">
-      <svg width="1100" height="400">
+      <svg className="ellipse-path" width="1100" height="400">
         <path
-          d="M0,100 a550,150 0 1,0 1100,0 a550,150 0 1,0 -1100,0"
+          d="M0,200 a550,150 0 1,0 1100,0 a550,150 0 1,0 -1100,0"
           fill="none"
           stroke="green"
           stroke-dasharray="5,5"
         />
       </svg>
-      <Moon/>
+      <Moon />
     </div>
   );
 };
